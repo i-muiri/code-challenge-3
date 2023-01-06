9751c11282ededa496a4a976a0cafd8ae52109e1
i-muiri <ianmuiri99@gmail.com>
2023-01-06 18:45:33 +0300

    Fixed URL

--- movies.js
+++ movies.js
@@ -41,7 +41,7 @@ purchaseButton.addEventListener("click", () => {
 });
 
 function fetchData() {
-  fetch("https://my-json-server.typicode.com/i-muiri/moviesapi/", { method: "GET" })
+  fetch("https://my-json-server.typicode.com/i-muiri/moviesapi/db/", { method: "GET" })
     .then((response) => response.json())
     .then((data) => {
       movies = data;
